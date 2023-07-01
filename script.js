9747b4779907a15ca3ac5af71fab61d888fd4fd2
Hardeep Singh <hs0774@gmail.com>
2023-06-30 23:48:32 -0400

    tree node classes created, treecreate made and insert element created

---
+++ script.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(data){
+        this.left=null;
+        this.data=data;
+        this.right=null;
+    }
+}
+
+class Tree {
+    constructor(array) {
+      this.root = null;
+      if (Array.isArray(array) && array.length > 0) {
+        this.root = this.buildTree(array,0,array.length-1);
+      }
+    }
+    buildTree(array,start,end){
+        if (start>end){
+            return null;
+        }
+        let mid = Math.floor((start+end)/2);
+        let node = new Node(array[mid]);
+        node.left=this.buildTree(array,start,mid-1);
+        node.right=this.buildTree(array,mid+1,end);
+        return node;
+    }
+    insertElement(data){
+        this.root = this.insertRecursive(this.root, data);
+    }
+    insertRecursive(node,data){
+        if(node===null){
+            return new Node(data);
+        }
+        if (data < node.data) {
+            node.left = this.insertRecursive(node.left, data);
+        } else if (data > node.data) {
+            node.right = this.insertRecursive(node.right, data);
+        }
+    
+        return node;
+    }
+    deleteElement(value){
+
+    }
+    find(value){
+
+    }
+}
